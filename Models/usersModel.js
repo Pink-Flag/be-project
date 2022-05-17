71e6348eb1f1091eb60a8391e02a04baabf1568f
Matthew Johnson <SuburbanHome@Matthews-iMac.local>
2022-05-17 14:41:33 +0100

    added extra keys in test

--- Models/usersModel.js
+++ Models/usersModel.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection.js");
 
 exports.selectUsers = () => {
-  return db.query(`SELECT username FROM users`).then((topics) => {
+  return db.query(`SELECT * FROM users`).then((topics) => {
     return topics.rows;
   });
 };
