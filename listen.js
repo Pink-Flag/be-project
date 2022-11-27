86c6b2770907720df334fc729b56d58e68aadec7
Matthew Johnson <mj@suburbanhome.co.uk>
2022-11-27 20:45:23 +0000

    fix node env

--- listen.js
+++ listen.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, (err) => {
+  console.log("hello matt");
   if (err) throw err;
   console.log(`Listening on ${PORT}...`);
 });
